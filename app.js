31c89f991e6820353298e38a395f0bc1bb6dc799
Issarapong Poesua <dekablade01@gmail.com>
2017-10-26 21:57:35 +0700

    move to app.js

--- app.js
+++ app.js
@@ -4,23 +4,53 @@
 
 var express = require("express");
 var fs = require("fs");
-
+var path = require('path');
+var multer = require("multer");
+var bodyParser = require("body-parser");
+var uploadPath = "./uploads/"
 var app = express();
 
 var port = process.env.PORT || 5000;
 
-var uploadRouter = require("./routes/upload");
 
-app.set('views', './views');
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.get("/", (req, res) => {
     res.render("index", { title: "welcome" });
 });
 
-app.use("/uploads", uploadRouter);
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, uploadPath)
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now() + '.jpg')
+    }
+});
+
+var upload = multer({ storage: storage }).fields
+
+
+app.post('/uploads', (req, res) => {
+    upload(req, res, (err) => {
+        if (!fs.existsSync(uploadPath)) {
+            fs.mkdirSync(uploadPath)
+        }
+
+        if (err) {
+            return res.json(err)
+        }
+
+        res.json({ success: true, message: 'Image uploaded!' });
+
+    });
+});
 
 
 app.listen(port, () => {
    console.log("application is listening on:", port);
 });
+
+module.exports = app;
